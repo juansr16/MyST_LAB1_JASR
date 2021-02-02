617182e661ba875486f2be55bfd245e6890d66e0
Juan S <juanandrees@hotmail.com>
2021-02-01 23:43:57 -0600

    avances de proyecto

--- data.py
+++ data.py
@@ -1,19 +1,11 @@
 import pandas as pd
-import numpy as np
 import yfinance as yf
 
-# import data from csv file
-data = pd.read_csv('C:/Users/JASR/Documents/Semestre Primavera 2021/Microestructura y Sistemas de '
-                   'Trading/MyST_LAB1_JFME/files/naftrac_holdings_dec_2020/NAFTRAC_holdings/NAFTRAC_310118.csv',
-                   skiprows=[0, 1])
+from os import path
 
-# Get tickers
-tickers = data['Ticker'].tolist()
-for i in range(0, len(tickers)):
-    tickers[i] = tickers[i]+'.MX'
-
-# Get weighing
-weight = data['Peso (%)'].tolist()
-for i in range(0, len(weight)):
-    weight[i] = weight[i] / 100
-
+# Set path
+abspath = path.abspath('C:/Users/JASR/Documents/Semestre Primavera 2021/Microestructura y Sistemas de '
+                       'Trading/MyST_LAB1_JFME/files/naftrac_holdings_dec_2020/NAFTRAC_holdings/')
+# Get data
+data = pd.read_csv(abspath + '/NAFTRAC_310118.csv', skiprows=[0, 1])
+data = data.dropna()
--- functions.py
+++ functions.py
@@ -1,10 +1,38 @@
+import pandas as pd
+import yfinance as yf
+import data as dt
 
-"""
-# -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
-# -- script: functions.py : python script with general functions                                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
-# -- license: GPL-3.0 License                                                                            -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
-# -- --------------------------------------------------------------------------------------------------- -- #
-"""
+# Get tickers
+tickers = dt.data['Ticker']
+for i in range(0, len(tickers)):
+    tickers[i] = tickers[i] + '.MX'
+
+# Get weighing
+weight = dt.data['Peso (%)']
+for i in range(0, len(weight)):
+    weight[i] = weight[i] / 100
+
+# Union de tickers y pesos
+w = pd.concat([tickers, weight], axis=1)
+
+# Tickers malos y obtener su index para sumar su ponderacion
+tm = ['KOFL.MX', ]
+a = w[w['Ticker'] == tm[0]].index.values.astype(int)
+t = a[0]
+t = w.loc[t, :].tolist()
+w.loc[len(w) - 1, 'Peso (%)'] = float(w.loc[len(w) - 1, 'Peso (%)']) + t[1]
+
+# Limpiar datos
+tickers = tickers.str.replace('*', '')
+tickers = tickers.str.replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX')
+tickers = tickers.str.replace('MEXCHEM.MX', 'ORBIA.MX')
+tickers = tickers.str.replace('GFREGIOO.MX', 'RA.MX')
+tickers = tickers.str.replace('MXN.MX', 'CASH')
+tickers = tickers.drop([10])
+w = w.drop([10])
+
+tickers = tickers.tolist()
+
+# Get historical data
+p = yf.download(tickers[0:len(tickers)], start="2018-01-31", end="2021-01-15")
+p = p.dropna()
